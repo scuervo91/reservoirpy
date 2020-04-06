afd9979dfdd639892480202c3cfd73d6282e3bbe
scuervo91 <scuervo91@gmail.com>
2020-04-05 23:07:27 -0500

    add correlation functions

--- reservoirpy/pvtpy/black_oil/__init__.py
+++ reservoirpy/pvtpy/black_oil/__init__.py
@@ -1,2 +1,2 @@
 from .pvt import oil_pvt
-from .correlations import n2_correction, co2_correction, h2s_correction, pb_standing
+from .correlations import n2_correction, co2_correction, h2s_correction, pb, rs, bo
