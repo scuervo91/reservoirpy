6463cae5043d4dac2cdabc6dccea76cb436d2af1
scuervo91 <scuervo91@gmail.com>
2020-04-26 02:09:21 -0500

    add cell functions

--- reservoirpy/simulationpy/grid/grid.py
+++ reservoirpy/simulationpy/grid/grid.py
@@ -11,11 +11,11 @@ def cell_id(i,j,k,nx,ny):
     """
     Get the cell Id given i,j,k indexes. 
         * ---  *  ---  *  --- *
-        | 0,2  |  1,2  |  2,2 |  <- Cell 6,7,8
+        | 0,2  |  1,2  |  2,2 |  <- Cell id 6,7,8
         * ---  *  ---  *  --- *
-        | 0,1  |  1,1  |  2,1 |  <- Cell 3,4,5
+        | 0,1  |  1,1  |  2,1 |  <- Cell id 3,4,5
         * ---  *  ---  *  --- *
-        | 0,0  |  1,0  |  2,0 |  <- Cell 0,1,2
+        | 0,0  |  1,0  |  2,0 |  <- Cell id 0,1,2
         * ---  *  ---  * ---  *
     """
     cell = (nx*j+i)+k*nx*ny
@@ -26,11 +26,11 @@ def cell_ijk(cell_id,nx,ny):
     """
     Get the cell indexes i,j,k given the cell id
         * ---  *  ---  *  --- *
-        | 0,2  |  1,2  |  2,2 |  <- Cell 6,7,8
+        | 0,2  |  1,2  |  2,2 |  <- Cell id 6,7,8
         * ---  *  ---  *  --- *
-        | 0,1  |  1,1  |  2,1 |  <- Cell 3,4,5
+        | 0,1  |  1,1  |  2,1 |  <- Cell id 3,4,5
         * ---  *  ---  *  --- *
-        | 0,0  |  1,0  |  2,0 |  <- Cell 0,1,2
+        | 0,0  |  1,0  |  2,0 |  <- Cell id 0,1,2
         * ---  *  ---  * ---  *
     """
     k=math.ceil(cell_id/(nx*ny))-1
@@ -51,6 +51,36 @@ def interpolate_z_pillar(z,p):
 
     xyz = np.array([x,y,z])
     return xyz
+
+#3D Rotation funtions
+def rotation(points,azimuth,dip,plunge):
+    assert points.ndim == 2
+
+    azi_rad = np.radians(azimuth)
+    dip_rad = np.radians(dip)
+    plg_rad = np.radians(plunge)
+
+    ry = np.array([
+        [np.cos(plg_rad),0,-np.sin(plg_rad)],
+        [0,1,0],
+        [np.sin(plg_rad),0,np.cos(plg_rad)],
+    ])
+    rx = np.array([
+        [1,0,0],
+        [0,np.cos(dip_rad),np.sin(dip_rad)],
+        [0,-np.sin(dip_rad),np.cos(dip_rad)]
+    ])
+    rz = np.array([
+        [np.cos(azi_rad),-np.sin(azi_rad),0],
+        [np.sin(azi_rad),np.cos(azi_rad),0],
+        [0,0,1]
+    ])
+
+    rot = np.matmul(np.matmul(ry,rx),rz)
+
+    rot_points = np.matmul(points,rot)
+
+    return rot_points
 
 
 
@@ -78,7 +108,10 @@ class grid():
         self.dy = kwargs.pop('dy',None)
         self.dz = kwargs.pop('dz',None)
         self.tops = kwargs.pop('tops',None)  #Pending to define
-        self.origin = kwargs.pop('origin',None)
+        self.origin = kwargs.pop('origin',Point(0,0,0))
+        self.azimuth = kwargs.pop('azimuth',0) #counterclockwise
+        self.dip = kwargs.pop('dip',0) #clockwise
+        self.plunge = kwargs.pop('plunge',0) #clockwise
 
         #Corner Point Grid
         self.coord = kwargs.pop('coord',None)
@@ -236,6 +269,102 @@ class grid():
             assert value.has_z, 'Point must have x,y,z coordinates'
         self._origin = value
 
+    @property
+    def azimuth(self):
+        return self._azimuth
+
+    @azimuth.setter
+    def azimuth(self,value):
+        assert isinstance(value,(int,float,np.ndarray)), 'Must be a number'
+
+        if isinstance(value,np.ndarray):
+            _az = value.flatten(order='F')
+            assert len(_az) == 1, f'list must length of 1'
+        
+        assert value >= -360 and value <= 360, 'Azimuth angle must be between 0 and 360'
+        self._azimuth = value
+
+    @property
+    def dip(self):
+        return self._dip
+
+    @dip.setter
+    def dip(self,value):
+        assert isinstance(value,(int,float,np.ndarray)), 'Must be a number'
+
+        if isinstance(value,np.ndarray):
+            _az = value.flatten(order='F')
+            assert len(_az) == 1, f'list must length of 1'
+        
+        assert value >= -360 and value <= 360, 'dip angle must be between 0 and 360'
+        self._dip = value
+
+    @property
+    def plunge(self):
+        return self._plunge
+
+    @plunge.setter
+    def plunge(self,value):
+        assert isinstance(value,(int,float,np.ndarray)), 'Must be a number'
+
+        if isinstance(value,np.ndarray):
+            _az = value.flatten(order='F')
+            assert len(_az) == 1, f'list must length of 1'
+        
+        assert value >= -360 and value <= 360, 'plunge angle must be between 0 and 360'
+        self._plunge = value
+
+    @property
+    def cartesian_vertices_coord(self):
+        #Vertices coordinates starting at 0,0,0
+        x_vert_cord = np.concatenate((np.zeros(1),self.dx.reshape((self.nx,self.ny,self.nz),order='f')[:,0,0]),axis=0).cumsum()
+        y_vert_cord = np.concatenate((np.zeros(1),self.dy.reshape((self.nx,self.ny,self.nz),order='f')[0,:,0]),axis=0).cumsum()
+        z_vert_cord = np.concatenate((np.zeros(1),self.dz.reshape((self.nx,self.ny,self.nz),order='f')[0,0,:]),axis=0).cumsum()
+
+        points = np.zeros(((self.nx+1)*(self.ny+1)*(self.nz+1),3))
+        for k in range(self.nz+1):
+            for j in range(self.ny+1):
+                for i in range(self.nx+1):
+                    l = cell_id(i,j,k,self.nx+1,self.ny+1)
+                    points[l,0] = x_vert_cord[i]
+                    points[l,1] = y_vert_cord[j]
+                    points[l,2] = z_vert_cord[k]
+
+        #Get rotated points with respect 0,0,0
+        rot_points = rotation(points,self.azimuth,self.dip,self.plunge)
+        
+        #Adjust the coordinates according with Origin Point
+        origin = np.array([self.origin.x,self.origin.y,self.origin.z])
+
+        self._vertices_coord = rot_points + origin
+        return self._vertices_coord
+
+    @property
+    def cartesian_center_point_coord(self):
+        #Vertices coordinates starting at 0,0,0
+        x_vert_cord = np.concatenate((np.zeros(1),self.dx.reshape((self.nx,self.ny,self.nz),order='f')[:,0,0]),axis=0).cumsum()
+        y_vert_cord = np.concatenate((np.zeros(1),self.dy.reshape((self.nx,self.ny,self.nz),order='f')[0,:,0]),axis=0).cumsum()
+        z_vert_cord = np.concatenate((np.zeros(1),self.dz.reshape((self.nx,self.ny,self.nz),order='f')[0,0,:]),axis=0).cumsum()
+
+        center = np.zeros(((self.nx)*(self.ny)*(self.nz),3))
+        for k in range(self.nz):
+            for j in range(self.ny):
+                for i in range(self.nx):
+                    l = cell_id(i,j,k,self.nx,self.ny)
+                    center[l,0] = np.mean((x_vert_cord[i], x_vert_cord[i+1]))
+                    center[l,1] = np.mean((y_vert_cord[j], y_vert_cord[j+1]))
+                    center[l,2] = np.mean((z_vert_cord[k], z_vert_cord[k+1]))
+
+        #Get rotated points with respect 0,0,0
+        rot_points = rotation(center,self.azimuth,self.dip,self.plunge)
+        
+        #Adjust the coordinates according with Origin Point
+        origin = np.array([self.origin.x,self.origin.y,self.origin.z])
+
+        self._center_coord = rot_points + origin
+        return self._center_coord
+
+
     # Corner Point Grid
     #---COORD---
     @property
@@ -315,10 +444,30 @@ class grid():
 ############## Methods ###########################
 
     def get_cell_id(self,i,j,k):
+        """
+        Get the cell Id given i,j,k indexes. 
+            * ---  *  ---  *  --- *
+            | 0,2  |  1,2  |  2,2 |  <- Cell id 6,7,8
+            * ---  *  ---  *  --- *
+            | 0,1  |  1,1  |  2,1 |  <- Cell id 3,4,5
+            * ---  *  ---  *  --- *
+            | 0,0  |  1,0  |  2,0 |  <- Cell id 0,1,2
+            * ---  *  ---  * ---  *
+        """
         c_id = cell_id(i,j,k,self.nx,self.ny)
         return c_id
 
     def get_cell_ijk(self,cell_id):
+        """
+        Get the cell indexes i,j,k given the cell id
+            * ---  *  ---  *  --- *
+            | 0,2  |  1,2  |  2,2 |  <- Cell id 6,7,8
+            * ---  *  ---  *  --- *
+            | 0,1  |  1,1  |  2,1 |  <- Cell id 3,4,5
+            * ---  *  ---  *  --- *
+            | 0,0  |  1,0  |  2,0 |  <- Cell id 0,1,2
+            * ---  *  ---  * ---  *
+        """
         i,j,k=cell_ijk(cell_id,self.nx,self.ny)
         return i,j,k
 
@@ -357,47 +506,170 @@ class grid():
         p0   p1
 
         """
-        p0 = cell_id(i,j,0,self.nx+1,self.ny+1)
-        p1 = cell_id(i+1,j,0,self.nx+1,self.ny+1)
-        p2 = cell_id(i,j+1,0,self.nx+1,self.ny+1)
-        p3 = cell_id(i+1,j+1,0,self.nx+1,self.ny+1)
+        if self.grid_type == 'corner_point':
+            p0 = cell_id(i,j,0,self.nx+1,self.ny+1)
+            p1 = cell_id(i+1,j,0,self.nx+1,self.ny+1)
+            p2 = cell_id(i,j+1,0,self.nx+1,self.ny+1)
+            p3 = cell_id(i+1,j+1,0,self.nx+1,self.ny+1)
 
-        pls = [self.get_pillar(p0),self.get_pillar(p1),self.get_pillar(p2),self.get_pillar(p3)]
-
+            pls = [self.get_pillar(p0),self.get_pillar(p1),self.get_pillar(p2),self.get_pillar(p3)]
+        else:
+            raise ValueError('Pillar are only set in a Corner Point Grid')
         return np.array(pls)
 
-    def get_corner_point_id(self,i,j,k):
-        nx,ny=2*self.nx,2*self.ny
-        p0=cell_id(2*i,2*j,2*k,nx,ny)
-        p1=cell_id(2*i+1,2*j,2*k,nx,ny)
-        p2=cell_id(2*i,2*j+1,2*k,nx,ny)
-        p3=cell_id(2*i+1,2*j+1,2*k,nx,ny)
+    def get_vertices_id(self,i,j,k):
+        """
+        Cartesian Grid
 
-        p4=cell_id(2*i,2*j,2*k+1,nx,ny)
-        p5=cell_id(2*i+1,2*j,2*k+1,nx,ny)
-        p6=cell_id(2*i,2*j+1,2*k+1,nx,ny)
-        p7=cell_id(2*i+1,2*j+1,2*k+1,nx,ny)
+        Get the cell Id given i,j,k indexes. 
+            13 --- 14  --- 15 --- 16
+            | 0,2  |  1,2  |  2,2 |  <- Cell id 6,7,8
+            9 ---  10  --- 11 --- 12
+            | 0,1  |  1,1  |  2,1 |  <- Cell id 3,4,5
+            5 ---  6  ---  7  --- 8
+            | 0,0  |  1,0  |  2,0 |  <- Cell id 0,1,2
+            1 ---  2  ---  3 ---  4
 
-        points = [p0,p1,p2,p3,p4,p5,p6,p7]
+        Corner Point Grid
 
-        return np.array(points)
+        3x3x1 system (2D X-Y plane)
+        30---31,32---33,34---35
+        |      |       |      |  <- Cell 6,7,8
+        24---25,26---27,28---29
+        18---19,20---21,22---23
+        |      |       |      |  <- Cell 3,4,5
+        12---13,14---15,16---17
+        6 --- 7,8 --- 9,10---11
+        |      |       |      |  <- Cell 0,1,2
+        0 --- 1,2 --- 3,4 --- 5
+        Node order convention for a 3D cell
+         6----7
+         -   -   <-Bottom Face
+        4----5
+          2----3
+         -    -  <-Top Face
+        0----1
+        """ 
+        if self.grid_type == 'cartesian':
+            nx,ny=self.nx+1,self.ny+1
+            p0=cell_id(i,j,k,nx,ny)
+            p1=cell_id(i+1,j,k,nx,ny)
+            p2=cell_id(i,j+1,k,nx,ny)
+            p3=cell_id(i+1,j+1,k,nx,ny)
 
-    def get_cell_z(self,i,j,k):
-        p = self.get_corner_point_id(i,j,k)
-        z = [self.zcorn[i] for i in p]
-        return np.array(z)
+            p4=cell_id(i,j,k+1,nx,ny)
+            p5=cell_id(i+1,j,k+1,nx,ny)
+            p6=cell_id(i,j+1,k+1,nx,ny)
+            p7=cell_id(i+1,j+1,k+1,nx,ny)
 
-    def get_cell_coords(self,i,j,k):
+            points = [p0,p1,p2,p3,p4,p5,p6,p7]
 
-        coords=[]
-        pillars = self.get_cell_pillars(i,j)
-        cell_z =self.get_cell_z(i,j,k)
+            return np.array(points)
 
-        for i in range(8):
-            p_id = i%4
-            coords.append(interpolate_z_pillar(cell_z[i],pillars[p_id]))
+        if self.grid_type == 'corner_point':
+            nx,ny=2*self.nx,2*self.ny
+            p0=cell_id(2*i,2*j,2*k,nx,ny)
+            p1=cell_id(2*i+1,2*j,2*k,nx,ny)
+            p2=cell_id(2*i,2*j+1,2*k,nx,ny)
+            p3=cell_id(2*i+1,2*j+1,2*k,nx,ny)
 
-        return np.array(coords)
+            p4=cell_id(2*i,2*j,2*k+1,nx,ny)
+            p5=cell_id(2*i+1,2*j,2*k+1,nx,ny)
+            p6=cell_id(2*i,2*j+1,2*k+1,nx,ny)
+            p7=cell_id(2*i+1,2*j+1,2*k+1,nx,ny)
+
+            points = [p0,p1,p2,p3,p4,p5,p6,p7]
+
+            return np.array(points)
+
+
+    def get_vertices_z(self,i,j,k):
+        # Get the z coord for a cell
+        if self.grid_type == 'corner_point':
+            p = self.get_vertices_id(i,j,k)
+            z = [self.zcorn[i] for i in p]
+            return np.array(z)
+        elif self.grid_type == 'cartesian':
+            p= self.get_vertices_id(i,j,k)
+            z = [self.cartesian_vertices_coord[i,2] for i in p]
+            return np.array(z)
+
+        # Pending for cartessian grid
+
+    def get_vertices_coords(self,i,j,k):
+        if self.grid_type == 'corner_point':
+            coords=[]
+            pillars = self.get_cell_pillars(i,j)
+            cell_z =self.get_vertices_z(i,j,k)
+
+            for i in range(8):
+                p_id = i%4
+                coords.append(interpolate_z_pillar(cell_z[i],pillars[p_id]))
+
+            return np.array(coords)
+        elif self.grid_type == 'cartesian':
+            p= self.get_vertices_id(i,j,k)
+            coords = [[self.cartesian_vertices_coord[i,0],self.cartesian_vertices_coord[i,1],self.cartesian_vertices_coord[i,2]] for i in p]
+            return np.array(coords)
+
+
+    def get_vertices_face_z(self,i,j,k, face=None):
+        """
+         Get the Z coords for a cell
+        
+         6----7
+         -   -   <-Bottom Face
+        4----5
+          2----3
+         -    -  <-Top Face
+        0----1   
+        Follow getCornerPointCellIdx convention:
+        X-, [0,2,4,6]
+        X+, [1,3,5,7]
+        Y-, [0,1,4,5]
+        Y+, [2,3,6,7]
+        Z+, [0,1,2,3]
+        Z-, [4,5,6,7]
+        """
+        assert face is not None, 'A face must be choosen'
+        points_id = self.get_vertices_id(i,j,k)
+        if(face=="X-"): face_id=[points_id[0],points_id[2],points_id[4],points_id[6]]
+        if(face=="X+"): face_id=[points_id[1],points_id[3],points_id[5],points_id[7]]
+        if(face=="Y-"): face_id=[points_id[0],points_id[1],points_id[4],points_id[5]]
+        if(face=="Y+"): face_id=[points_id[2],points_id[3],points_id[6],points_id[7]]
+        if(face=="Z-"): face_id=[points_id[4],points_id[5],points_id[6],points_id[7]]
+        if(face=="Z+"): face_id=[points_id[0],points_id[1],points_id[2],points_id[3]]
+
+        if self.grid_type == 'cartesian':
+            z_face = [self.cartesian_vertices_coord[i,2] for i in face_id]
+        elif self.grid_type == 'corner_point':
+            z_face = [self.zcorn[i] for i in face_id]
+
+        return np.array(z_face)
+
+    def get_center_coord(self,i,j,k):
+        """
+        Get the cell Id given i,j,k indexes. 
+            * ---  *  ---  *  --- *
+            | 0,2  |  1,2  |  2,2 |  <- Cell id 6,7,8
+            * ---  *  ---  *  --- *
+            | 0,1  |  1,1  |  2,1 |  <- Cell id 3,4,5
+            * ---  *  ---  *  --- *
+            | 0,0  |  1,0  |  2,0 |  <- Cell id 0,1,2
+            * ---  *  ---  * ---  *
+        """
+        cid = self.get_cell_id(i,j,k)
+        if self.grid_type == 'cartesian':
+            center = self.cartesian_center_point_coord[cid,:]
+        elif self.grid_type == 'corner_point':
+            points = self.get_vertices_coords(i,j,k)
+            center = points.mean(axis=0)
+        
+        return center
+
+
+
+
 
         
 
