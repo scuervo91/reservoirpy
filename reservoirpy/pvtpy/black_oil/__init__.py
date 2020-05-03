396a67662c34e698a882a6603e2c3d8d089aa786
scuervo91 <scuervo91@gmail.com>
2020-05-02 22:27:44 -0500

    add new functions and classes

--- reservoirpy/pvtpy/black_oil/__init__.py
+++ reservoirpy/pvtpy/black_oil/__init__.py
@@ -1,2 +1,3 @@
-from .pvt import oil_pvt,oil, water_pvt,gas_pvt
-from .correlations import n2_correction, co2_correction, h2s_correction, pb, rs, bo, rho_oil, co, muod, muo,rsw, bw,cw
+from .pvt import oil_pvt, oil, water_pvt, gas_pvt, properties_df,chromatography
+from .correlations import n2_correction, co2_correction, h2s_correction, pb, rs, \
+    bo, rho_oil, co, muod, muo,rsw, bw, cw, muw, rhow, rhog, z_factor, bg
