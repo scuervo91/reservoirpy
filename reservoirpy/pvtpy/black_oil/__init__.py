3b22f1dcd63d3f67af14c88cac398c0509bdae76
scuervo91 <scuervo91@gmail.com>
2020-05-07 20:44:58 -0500

    add new correlation functions

--- reservoirpy/pvtpy/black_oil/__init__.py
+++ reservoirpy/pvtpy/black_oil/__init__.py
@@ -1,3 +1,4 @@
-from .pvt import oil_pvt, oil, water_pvt, gas_pvt, properties_df,chromatography
+from .pvt import oil_pvt, oil, water_pvt, gas_pvt, properties_df,chromatography, gas
 from .correlations import n2_correction, co2_correction, h2s_correction, pb, rs, \
-    bo, rho_oil, co, muod, muo,rsw, bw, cw, muw, rhow, rhog, z_factor, bg
+    bo, rho_oil, co, muod, muo,rsw, bw, cw, muw, rhow, rhog, z_factor, bg, eg, critical_properties,\
+        critical_properties_correction, cg
