3a8d938d75fb8dc55cbfb6e05a4703f53df7350c
scuervo91 <scuervo91@gmail.com>
2020-05-25 17:07:10 -0500

    add fm track

--- reservoirpy/welllogspy/tracks/fmtrack.py
+++ reservoirpy/welllogspy/tracks/fmtrack.py
@@ -2,20 +2,46 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def fmtrack(fm=None,lims=None, top='top',bottom='bottom',name='name',ax=None):
+def fmtrack(df,
+            lims:list = None,
+            depth_ref:str = 'md',
+            area_kw:dict = {},
+            line_kw:dict = {},
+            ax=None
+            ):
     
+    
+    #fm=None,lims=None, top='top',bottom='bottom',name='name',ax=None):
+
+    # Default kwargs for area
+    def_area_kw = {
+    'xmin': 0,
+    'xmax': 1,
+    'alpha': 0.2
+    }
+    for (k,v) in def_area_kw.items():
+        if k not in area_kw:
+            area_kw[k]=v
+
+    # Default kwargs for lines
+    def_line_kw = {
+        'linewidth': 1
+    }
+    for (k,v) in def_line_kw.items():
+        if k not in line_kw:
+            line_kw[k]=v
+
     fax=ax or plt.gca()
         
-    fm['mid']=fm.mean(axis=1)
     if lims==None: #Depth Limits
-        lims=[fmf.loc[:,top].max(),fmf.loc[:,top].max()]
+        lims=[df.loc[:,f'{depth_ref}_top'].min(),df.loc[:,f'{depth_ref}_bottom'].max()]
     fax.set_ylim([lims[1],lims[0]])
-    fmf=fm[(fm['mid']<=lims[1]) & (fm['mid']>=lims[0])]
-    m=fmf.shape[0]
+
+    dff = df[(df[f'{depth_ref}_top']<=lims[1]) & (fm[f'{depth_ref}_bottom']>=lims[0])]
     
-    for i in fmf.iterrows():
-        fax.axhspan(i[1][top],i[1][bottom],xmin=0,xmax=1,alpha=0.2)
-        fax.hlines([i[1][top],i[1][bottom]],0,1, linewidth=1.0)
+    for i in dff.iterrows():
+        fax.axhspan(i[1][f'{depth_ref}_top'],i[1][f'{depth_ref}_bottom'],**area_kw)
+        fax.hlines([i[1][f'{depth_ref}_top'],i[1][f'{depth_ref}_bottom']],0,1, line_kw)
         
-    fax.set_yticks(fmf.mid)
-    fax.set_yticklabels(fmf[name])
+    fax.set_yticks(dff[f'{depth_ref}_top'])
+    fax.set_yticklabels(dff['formation'])
